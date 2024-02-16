644f6454580cbba4ac5d8256ef8c6bb95ce023a1
PrivateForrestGump <forrestleung58@gmail.com>
2024-02-16 12:26:27 +0800

    ea sports

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ Q1:
 '''
 
 # TODO: Write your code here
+for e in range(7):
+    for i in range(e+1):
+        print(i, end=' ')
 
+    print()
 ##################################################
 '''
 Q2:
@@ -12,19 +16,32 @@ Q2:
 
 # TODO: Write your code here
 
+for e in range(6):
+    for i in range(e+1):
+        print(i+1, end=' ')
+
+    print()
 ##################################################
 '''
 Q3:
 '''
 
 # TODO: Write your code here
-
+for i in range(9):
+    for e in range(5):
+        print(9-i, end= '')
+        
+print()
 ##################################################
 '''
 Q4:
 '''
 
 # TODO: Write your code here
+for i in range(1, 10, 2):
+    for e in range(i):
+        print(i, end= '')
+    print()
 
 ##################################################
 '''
@@ -32,12 +49,24 @@ Q5:
 '''
 
 # TODO: Write your code here
-
+for i in range(5):
+    for e in range(5-i):
+        print('-', end= '')
+    for e in range(2*i+1):
+        print(2*i+1, end= '')
+    print()
 ##################################################
 '''
 Q6:
 '''
 
 # TODO: Write your code here
-
+for i in range(5):
+    for e in range(5-i):
+        print(' ', end= '')
+    for e in range(i+1):
+        print(e+1, end= '')
+    for e in range(i):
+       print(i-e, end= '')
+    print()
 ##################################################
